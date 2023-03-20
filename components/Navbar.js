2f8d772c9f01fbab4339c24aa766cbdee536ceca
renzgabrinao <ryan.gabrinao@gmail.com>
2023-03-19 18:53:08 -0700

    updated width

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed left-0 z-50 grid grid-cols-3 w-[85%] mx-auto right-0 px-4 py-3 font-normal tracking-tighter bottom-4 text-rg-white font-satoshi text-step4 md:bottom-[unset] md:top-0 glassmorphism rounded-full">
+    <nav className="fixed left-0 z-50 grid grid-cols-3 w-[95%] mx-auto right-0 px-6 py-3 font-normal tracking-tighter bottom-4 text-rg-white font-satoshi text-step4 md:bottom-[unset] md:top-0 glassmorphism rounded-full">
       <Link href="/" className="hover:italic w-fit">
         Home
       </Link>
