3444753a5f97a58446255e4b160e7b7d594c5351
renzgabrinao <ryan.gabrinao@gmail.com>
2023-03-19 18:50:03 -0700

    added content to single project page

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
 
-const Button = ({ primary, href, text }) => {
+const Button = ({ primary, href, text, disabled }) => {
   return (
     <Link
       href={href}
-      className={`py-2 px-6 rounded-full font-semibold shadow-sm shadow-black ${
+      className={`py-2 px-6 rounded-full text-step5 font-semibold shadow-sm shadow-black ${
         primary ? "bg-rg-white text-primary" : "border text-rg-white"
-      }`}
+      } ${disabled && "opacity-20 pointer-events-none"}`}
+      disabled={disabled ? "true" : "false"}
     >
       {text}
     </Link>
--- pages/works/[slug].js
+++ pages/works/[slug].js
@@ -3,10 +3,13 @@ import React from "react";
 import Image from "next/image";
 import urlFor from "@/utils/imageBuilder";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { motion as m } from "framer-motion";
 import Button from "@/components/Button";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
 
 const Single = ({ project }) => {
+  const router = useRouter();
   return (
     <m.main
       className="min-h-screen font-neuehaas text-rg-white"
@@ -18,6 +21,12 @@ const Single = ({ project }) => {
       }}
     >
       <section id="hero" className="relative">
+        <button
+          className="absolute z-10 top-4 left-1"
+          onClick={() => router.back()}
+        >
+          <ArrowUturnLeftIcon className="w-6 h-6" />
+        </button>
         <h1 className="font-bold absolute bottom-0 z-10 text-step_2 left-2">
           {project.title}
         </h1>
@@ -30,14 +39,15 @@ const Single = ({ project }) => {
           />
         </div>
       </section>
-      <section id="overview" className="font-satoshi px-5">
-        <div className="rounded-lg py-16 flex flex-col gap-4">
+      <section id="overview" className="font-satoshi px-5 my-14">
+        <div className=" flex flex-col gap-4 pb-14">
           <h2 className="font-bold text-step2 tracking-tighter">Overview</h2>
           <p className="opacity-70 bg-gray-800 rounded-lg p-4 text-step5 shadow-md shadow-black">
             {project.excerpt}
           </p>
         </div>
-        <div className="border-t-2 py-16 border-rg-white border-opacity-20 flex flex-col gap-8">
+
+        <div className="border-t-2 border-rg-white border-opacity-20 flex flex-col gap-8 py-14">
           <div className="flex justify-between">
             <h2 className="font-bold text-step4 tracking-tighter">Client</h2>
             <p className="opacity-70 text-step5">N/A</p>
@@ -60,6 +70,25 @@ const Single = ({ project }) => {
           <Button href={`/`} text="Live Site" primary />
         </div>
       </section>
+      <section className="font-satoshi px-5 mb-14">
+        <div className="flex flex-col gap-4">
+          <h2 className="font-bold text-step2 tracking-tighter">Process</h2>
+          <p className="opacity-70 bg-gray-800 rounded-lg p-4 text-step5 shadow-md shadow-black">
+            {project.excerpt}
+          </p>
+        </div>
+      </section>
+      <section className="font-satoshi px-5 mb-14">
+        <div className="flex flex-col gap-4">
+          <h2 className="font-bold text-step2 tracking-tighter">
+            Functionality
+          </h2>
+          <p className="opacity-70 bg-gray-800 rounded-lg p-4 text-step5 shadow-md shadow-black">
+            {project.excerpt}
+          </p>
+        </div>
+      </section>
+      <section>Heloo</section>
     </m.main>
   );
 };
